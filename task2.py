da3ea3242e63a319f1d85b964b8c9111ea911ea7
Galukhin <Lutiyroker@yandex.ru>
2020-10-19 21:02:56 +0300

    Update task2 and add task6

--- task2.py
+++ task2.py
@@ -4,14 +4,8 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input().
 """
-my_list = []
-print("Добавьте что-нибудь в список. Для окончания нажмите q")
-item = 0
-while True:
-    item = input(">>>")
-    if item == "q":
-        break
-    my_list.append(item)
+user_str = input("Введите список, разделяя элементы списка пробелом:\n>>> ")
+my_list = user_str.split()
 
 for i in range(len(my_list) // 2):
     my_list[i * 2], my_list[i * 2 + 1] = my_list[i * 2 + 1], my_list[i * 2]
